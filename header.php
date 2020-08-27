379512f796c3d03a20bb92f62d7450e8e9b78761
ben <info@gmail.com>
2020-08-27 15:02:13 +0530

    text change

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
-	<?php wp_head(); ?> oh bhai kya kar rhe ho
+	<?php wp_head(); ?> Lets write something decent
 </head>
 
 <body <?php body_class(); ?>>
